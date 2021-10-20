5006c7ae0dd27bca81a82aba976a964b21310dac
Pavlo Makovetskyi <75490002+PavloMakovetskyi@users.noreply.github.com>
2021-10-20 23:36:59 +0300

    Add files via upload

---
+++ lab01/TaskOne/1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+
+            x = double.Parse(ReadLine()); y = double.Parse(ReadLine()); z = double.Parse(ReadLine());
+
+            if (z == 0)
+            {
+                WriteLine("Wrong input");
+                System.Environment.Exit(1);
+            }
+
+            double a = Cos(x + ((x * y) / z));
+
+            if (Cos(a) == 0)
+            {
+                WriteLine("Zero division!");
+                System.Environment.Exit(1);
+            }
+
+            double b = Cos((x * x * x) / Cos(a));
+
+            WriteLine(a.ToString() + " " + b.ToString());
+        }
+    }
+}
