681a55416a316fa1b14d9709ea76815ca1951621
Pavlo Makovetskyi <75490002+PavloMakovetskyi@users.noreply.github.com>
2021-10-20 23:59:02 +0300

    Add files via upload

---
+++ lab01/TaskOne/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+namespace ADS_Lab01_CodeEnv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(ReadLine()); double y = double.Parse(ReadLine()); double z = double.Parse(ReadLine());
+            double a, b, quadratic_root = 0;
+
+            if ((x * x * x) + 5 * Pow(y, -z) + (z * z) <= 0)
+            {
+                WriteLine("Zero division");
+                System.Environment.Exit(1);
+            }
+            else
+            {
+                quadratic_root = Sqrt((x * x * x) + 5 * Pow(y, -z) + (z * z));
+            }
+
+            a = (y - Sqrt(Abs(x * x * x))) / quadratic_root;
+
+            b = Sin(Pow(a, -x) + y);
+
+            WriteLine(a.ToString() + " " + b.ToString());
+        }
+    }
+}
